32140b1d101f62035e0b20aba946d5ee96f350e4
techman <dmoloney5@gmail.com>
2022-04-02 11:29:47 -0400

    users

--- models/index.js
+++ models/index.js
@@ -1,6 +1,6 @@
 // import all models
 const Post = require("./Post");
-const User = require("./User");
+const User = require("./Users");
 const Vote = require("./Vote");
 const Comment = require("./Comment");
 
